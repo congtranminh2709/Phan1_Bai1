a0fe52d67f7447034e4d363089e3ba2a0c10e1ea
congtran <congtranminh2001@gmail.com>
2023-01-05 14:42:14 +0700

    Add project files.

---
+++ Phan2_Bai2/Account.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phan2_Bai2
+{
+    internal class Account
+    {
+        public int AccountID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Balance { get; set; }
+
+        public Account() { }
+        public Account(int AccountID, string FirstName, string LastName, double Balance)
+        {
+            this.AccountID = AccountID;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Balance = Balance;
+        }
+        public int accountID { get => AccountID; set => AccountID = value; }
+        public string firstName { get => FirstName; set => FirstName = value; }
+        public string lastName { get => LastName; set => LastName = value; }
+        public double balance { get => Balance; set => Balance = value; }
+
+        public void Show()
+        {
+            Console.WriteLine("--------------------\n");
+            Console.WriteLine("AcountID: " + AccountID);
+            Console.WriteLine("FirstName: " + FirstName);
+            Console.WriteLine("LastName: " + LastName);
+            Console.WriteLine("Balance: " + Balance);
+        }
+        public void Input()
+        {
+            Console.Write("AccountID: ");
+            AccountID = int.Parse(Console.ReadLine());
+            Console.Write("FirstName: ");
+            FirstName = Console.ReadLine();
+            Console.Write("LastName: ");
+            LastName = Console.ReadLine();
+            Console.Write("Balance: ");
+            Balance = int.Parse(Console.ReadLine());
+
+        }
+    }
+}
---
+++ Phan2_Bai2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phan2_Bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AccountList accountList = new AccountList();
+            //accountList.InputList();
+            //accountList.ShowList();
+            //Console.ReadKey();
+            int choose = 0;
+            do
+            {
+                Console.Write("----------------------------------\n");
+                Console.WriteLine("1. New Account");
+                Console.WriteLine("2. Save File");
+                Console.WriteLine("3. Load File");
+                Console.WriteLine("4. Report");
+                Console.WriteLine("0. Exit");
+
+                Console.Write("Select: ");
+                choose = Convert.ToInt32(Console.ReadLine());
+
+                switch (choose)
+                {
+                    case 1:
+                        Console.Clear();
+                        accountList.InputList();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.Clear();
+                        accountList.SaveFile();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.Clear();
+                        accountList.LoadFile();
+                        Console.Clear();
+                        break;
+                    case 4:
+                        Console.Clear();
+                        accountList.ShowList();
+                        break;
+                    default:
+                        break;
+                }
+            } while (choose != 0);
+        }
+    }
+}
